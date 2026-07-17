8a83c436606e4e0f7400368afba9ad32d1b0b45a
NikitaAvrn <mel16bit@gmail.com>
2026-07-17 21:49:26 +0300

    feat(patterns): разработать реализацию паттерна 'цепочка обязанностей'

--- 3/HW/src/com/homework/Main.java
+++ 3/HW/src/com/homework/Main.java
@@ -1,5 +1,13 @@
 package com.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.homework.ChainOfResponsibility.BubbleWrapRobot;
+import com.homework.ChainOfResponsibility.LabelRobot;
+import com.homework.ChainOfResponsibility.Parcel;
+import com.homework.ChainOfResponsibility.WarehouseRobot;
+import com.homework.ChainOfResponsibility.WeightControlRobot;
 import com.homework.Strategy.DriftDrive;
 import com.homework.Strategy.EcoDrive;
 import com.homework.Strategy.ElectricCar;
@@ -8,24 +16,45 @@ import com.homework.Strategy.PowerDrive;
 public class Main {
     public static void main(String[] args) {
         strategyPattern();
+        chainOfResponsibilityPattern();
+    }
+
+    private static void chainOfResponsibilityPattern() {
+        WarehouseRobot conveyor = new WeightControlRobot();
+        conveyor.setNext(new BubbleWrapRobot())
+                .setNext(new LabelRobot());
+
+        List<Parcel> parcels = new ArrayList<Parcel>(List.of(
+                new Parcel("Хрустальная ваза", 2.5, true),
+                new Parcel("Смартфон ЯТелефон ПроМаркс", .5, true),
+                new Parcel("Наковальня", 55, false)));
+
+        for (Parcel parcel : parcels) {
+            System.out.println(String.format("--- %s ---", parcel.getTitle()));
+            if (conveyor.process(parcel)) {
+                System.err.println("Успех! Заказ отправлен клиенту");
+            } else {
+                System.err.println("Ошибка! Заказ отменен");
+            }
+        }
     }
 
     private static void strategyPattern() {
-        System.err.println("Strategy pattern");
+        System.out.println("Strategy pattern");
 
         ElectricCar tesla = new ElectricCar(new EcoDrive());
         tesla.pressAccelerate();
         tesla.releaseAccelerate();
-        System.err.println(tesla.engineInfo());
+        System.out.println(tesla.engineInfo());
 
-        System.err.println(tesla.changeDriveMode(new PowerDrive()));
+        System.out.println(tesla.changeDriveMode(new PowerDrive()));
         tesla.pressAccelerate();
         tesla.releaseAccelerate();
-        System.err.println(tesla.engineInfo());
+        System.out.println(tesla.engineInfo());
 
-        System.err.println(tesla.changeDriveMode(new DriftDrive()));
+        System.out.println(tesla.changeDriveMode(new DriftDrive()));
         tesla.pressAccelerate();
         tesla.releaseAccelerate();
-        System.err.println(tesla.engineInfo());
+        System.out.println(tesla.engineInfo());
     }
 }
